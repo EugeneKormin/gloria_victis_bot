b8a64cf48a040d30457d64df2e6eb582795512f9
eKormin <resolweru@mail.ru>
2023-03-09 17:34:40 +1000

    Add Keyboard.py Code structure was change to add ability to protect botself from looters

--- FishingBot.py
+++ FishingBot.py
@@ -1,9 +1,10 @@
+import time
+
 from Screen import Screen
 from Mouse import Mouse
-from Keys import Keys
+from Keyboard import Keyboard
 
 import pytesseract
-import time
 import cv2
 
 
@@ -11,7 +12,8 @@ class FishingBot(object):
     def __init__(self):
         self.__screen = Screen(WINDOW_NAME="Gloria Victis")
         self.__mouse = Mouse()
-        self.__keys = Keys()
+        self.__keyboard = Keyboard()
+        self.__IF_BOT_IS_SAFE = False
         self.__execute()
 
     def __detect_direction(self, screen):
@@ -21,26 +23,25 @@ class FishingBot(object):
         TEXT = pytesseract.image_to_string(text_image)
         return TEXT
 
-    def __start_fishing(self):
-        self.__keys.directKey("6", self.__keys.key_press)
-        time.sleep(0.04)
-        self.__keys.directKey("6", self.__keys.key_release)
+    def __prepare_for_fishing(self):
+        self.__keyboard.press_key_sequence(key_sequence=["6", "0", "e"])
 
-        time.sleep(1.5)
+    def __run_fishing_sequence(self):
+        screen = self.__screen.update_img()
+        DIRECTION = self.__detect_direction(screen=screen)
+        self.__mouse.move(DISTANCE=14, DIRECTION=DIRECTION)
 
-        self.__keys.directKey("0", self.__keys.key_press)
-        time.sleep(0.04)
-        self.__keys.directKey("0", self.__keys.key_release)
-
-        time.sleep(1.5)
-
-        self.__mouse.press_button()
+    def __check_if_bot_is_safe(self):
+        return True
 
     def __execute(self):
-        self.__start_fishing()
+        time.sleep(5)
         while True:
-            screen = self.__screen.update_img()
+            PREV_STATUS = self.__IF_BOT_IS_SAFE
+            self.__IF_BOT_IS_SAFE = self.__check_if_bot_is_safe()
 
-            DIRECTION = self.__detect_direction(screen=screen)
+            if PREV_STATUS is False and self.__IF_BOT_IS_SAFE:
+                self.__prepare_for_fishing()
 
-            self.__mouse.move(DISTANCE=14, DIRECTION=DIRECTION)
+            while self.__IF_BOT_IS_SAFE:
+                self.__run_fishing_sequence()
--- Mouse.py
+++ Mouse.py
@@ -24,34 +24,39 @@ class Mouse(object):
 
     def __move_down(self, DISTANCE):
         for i in range(DISTANCE):
-            self.__keys.directMouse(0, 1 * DISTANCE)
+            self.__keys.directMouse(0, 1 * i)
             sleep(random.uniform(0.03, 0.05))
 
     def __move_mouse(self, DISTANCE, DIRECTION):
         if "left" in DIRECTION:
             self.__move_left(DISTANCE=DISTANCE)
-            self.press_button()
+            self.press_left_button()
             self.__move_right(DISTANCE=DISTANCE)
 
         elif "right" in DIRECTION:
             self.__move_right(DISTANCE=DISTANCE)
-            self.press_button()
+            self.press_left_button()
             self.__move_left(DISTANCE=DISTANCE)
 
         elif "up" in DIRECTION:
             self.__move_up(DISTANCE=DISTANCE)
-            self.press_button()
+            self.press_left_button()
             self.__move_down(DISTANCE=DISTANCE)
 
         elif "down" in DIRECTION:
             self.__move_down(DISTANCE=DISTANCE)
-            self.press_button()
+            self.press_left_button()
             self.__move_up(DISTANCE=DISTANCE)
 
-    def press_button(self):
+    def press_left_button(self):
         self.__keys.directMouse(buttons=self.__keys.mouse_lb_press)
         sleep(random.uniform(0.06, 0.09))
         self.__keys.directMouse(buttons=self.__keys.mouse_lb_release)
+
+    def press_right_button(self):
+        self.__keys.directMouse(buttons=self.__keys.mouse_rb_press)
+        sleep(random.uniform(0.06, 0.09))
+        self.__keys.directMouse(buttons=self.__keys.mouse_rb_release)
 
     def move(self, DISTANCE: int, DIRECTION: str):
         self.__move_mouse(
